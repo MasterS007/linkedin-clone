5d36115778910f06effb6d380690493cc8461d9c
MasterS007 <munirasirajum13@gmail.com>
2022-08-25 15:55:54 +0600

    finally

--- pages/api/posts/index.js
+++ pages/api/posts/index.js
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
       res.status(200).json(posts);
     } catch (err) {
       // console.log(err);
-      res.status(500).json(err);
+      res.status(500).json({ message: err.message });
     }
   }
 
